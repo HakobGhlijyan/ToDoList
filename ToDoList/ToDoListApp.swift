0e937a9e79924cdb2ebeca08ac64a6f696c11f07
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-10 20:16:02 +0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Hakob Ghlijyan on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
