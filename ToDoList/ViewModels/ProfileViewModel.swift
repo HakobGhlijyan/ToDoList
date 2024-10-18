b8cca37629d237e0d80142629b43326fc724cc09
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-18 17:10:47 +0400

    Part13 End add log out add profile view info current user.

--- ToDoList/ViewModels/ProfileViewModel.swift
+++ ToDoList/ViewModels/ProfileViewModel.swift
@@ -6,7 +6,49 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 final class ProfileViewModel: ObservableObject {
     init() {}
+    
+    @Published var user: User? = nil
+    
+    // Polucheniw info user dlya otobrojeniya na UI screen view profile
+    func fetchUser() {
+        //fetch id
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        
+        //add dataBase for user
+        let db = Firestore.firestore()
+        
+        // poluchit document user
+        db.collection("users").document(userID).getDocument { [weak self] snapshot, error in
+            //poluchaem danie
+            guard let data = snapshot?.data(), error == nil else { return }
+            
+            //sozdayom user , v main potoke chob obnovlyat ego dlya UI na ekrane
+            DispatchQueue.main.async {
+                self?.user = User(
+                    id: data["id"] as? String ?? "", // v vide data s privideniem string tipa... ili on budet ""
+                    name: data["name"] as? String ?? "", // v vide data s privideniem string tipa... ili on budet "",
+                    email: data["email"] as? String ?? "", // v vide data s privideniem string tipa... ili on budet "",
+                    joinedDate: data["joinedDate"] as? TimeInterval ?? 0 // v vide data s privideniem TimeInterval tipa... ili on budet 0
+                )
+                
+                //etogo dostatochno chtob nayti nachego current user a ,
+            }
+        }
+    }
+    
+    // log out , vixod iz profilya
+    // Errors thrown from here are not handled !! Nam nujno gdeto xranit i obrobativat error
+    // v do catch blok zakidivaem
+    func logOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,10 +13,60 @@ struct ProfileView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome to ToDoList")
+                if let user = viewModel.user {
+                    profile(user: user)
+                    
+                } else {
+                    ContentUnavailableView("Loading Profile...", systemImage: "person.crop.circle", description: Text("Please wait while we load your profile."))
+                }
+                
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder func profile(user: User) -> some View {
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .scaledToFit()
+            .foregroundStyle(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        Text("Welcome \(user.name.capitalized)")
+        
+        //Info: Name , EMail, Member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ").bold()
+                Text(user.name.capitalized)
+            }
+            .padding()
+            HStack {
+                Text("Email: ").bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Memner Since: ").bold()
+                Text("\(Date(timeIntervalSince1970: user.joinedDate).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        // Sign Out
+        TLButtonView(title: "Log Out", backgroundColor: .red) {
+            viewModel.logOut()
+        }
+        .padding()
+        .padding(.horizontal, 64)
+
+        Spacer()
     }
 }
 
