ad421aba4e99fb7d03dee8a8d2ccc89eb9a30903
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-26 14:14:30 +0400

    Clean All code

--- ToDoList/ViewModels/ProfileViewModel.swift
+++ ToDoList/ViewModels/ProfileViewModel.swift
@@ -14,7 +14,6 @@ final class ProfileViewModel: ObservableObject {
     
     @Published var user: User? = nil
     
-    // Polucheniw info user dlya otobrojeniya na UI screen view profile
     func fetchUser() {
         //fetch id
         guard let userID = Auth.auth().currentUser?.uid else { return }
@@ -24,26 +23,19 @@ final class ProfileViewModel: ObservableObject {
         
         // poluchit document user
         db.collection("users").document(userID).getDocument { [weak self] snapshot, error in
-            //poluchaem danie
             guard let data = snapshot?.data(), error == nil else { return }
             
-            //sozdayom user , v main potoke chob obnovlyat ego dlya UI na ekrane
             DispatchQueue.main.async {
                 self?.user = User(
-                    id: data["id"] as? String ?? "", // v vide data s privideniem string tipa... ili on budet ""
-                    name: data["name"] as? String ?? "", // v vide data s privideniem string tipa... ili on budet "",
-                    email: data["email"] as? String ?? "", // v vide data s privideniem string tipa... ili on budet "",
-                    joinedDate: data["joinedDate"] as? TimeInterval ?? 0 // v vide data s privideniem TimeInterval tipa... ili on budet 0
+                    id: data["id"] as? String ?? "",
+                    name: data["name"] as? String ?? "",
+                    email: data["email"] as? String ?? "",
+                    joinedDate: data["joinedDate"] as? TimeInterval ?? 0
                 )
-                
-                //etogo dostatochno chtob nayti nachego current user a ,
             }
         }
     }
     
-    // log out , vixod iz profilya
-    // Errors thrown from here are not handled !! Nam nujno gdeto xranit i obrobativat error
-    // v do catch blok zakidivaem
     func logOut() {
         do {
             try Auth.auth().signOut()
--- ToDoList/ViewModels/ToDoListViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewModel.swift
@@ -14,8 +14,7 @@ final class ToDoListViewModel: ObservableObject {
     
     private let userId : String
     
-    init(userId: String) {       // tak mi v view modele pri init ego budem poluchat id is struct i v nego peredavat .
-                                 // a sdes on uje poluchv id budet delete
+    init(userId: String) {
         self.userId = userId
     }
 
@@ -29,7 +28,7 @@ final class ToDoListViewModel: ObservableObject {
         // delete todo is path collection users is document -> collection todos is document -> delete for id...
         db
             .collection("users")
-            .document(userId)    // Teper sdez kak path document u user est id i on etot pluchaet iz init v struct
+            .document(userId)   
             .collection("todos")
             .document(id)
             .delete()
